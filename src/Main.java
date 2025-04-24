bd206bec8dc01f51bddd64f61ad3636eedc38fdb
morata.najav <morata.najav24@cordoba.salesianos.edu>
2025-04-24 21:07:45 +0200

    El error era que no estaba controlado si no habia estuidantes al añadirle un NullPointerException, para que cuando salga el error nos de que no se han encontrado estudiantes.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Estudiante[] estudiantes = new Estudiante[]{
-//                new Estudiante("Ana", 20, new double[]{8.5, 9.0, 7.5}),
-//                new Estudiante("Luis", 22, new double[]{6.0, 5.5}),
-//                new Estudiante("Marta", 19, new double[]{9.5, 10.0}),
-//                new Estudiante("Carlos", 21, new double[]{}) // Error: sin notas
+                new Estudiante("Ana", 20, new double[]{8.5, 9.0, 7.5}),
+                new Estudiante("Luis", 22, new double[]{6.0, 5.5}),
+                new Estudiante("Marta", 19, new double[]{9.5, 10.0}),
+                new Estudiante("Carlos", 21, new double[]{}) // Error: sin notas
         };
 
         System.out.println("Calculando el mejor estudiante...");
